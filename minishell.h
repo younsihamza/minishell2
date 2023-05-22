5bce5db489c92840bac4ddaa7041eadebe91a0b3
Issame Chouaref <ichouare@e2r4p13.1337.ma>
2023-05-22 15:27:16 +0100

    adshf

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ichouare <ichouare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 16:05:38 by ichouare          #+#    #+#             */
-/*   Updated: 2023/05/22 12:38:19 by ichouare         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:26:40 by ichouare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 # include <fcntl.h>
 # include <signal.h>
 # include <sys/stat.h>
+
 int	g_s[4];
 typedef struct lexer
 {
@@ -194,9 +195,6 @@ void	ft_add_new_up(t_vars **declare, char *str, char *buffer1, char *data);
 char	*ft_strtrim(char  *s1, char  *set);
 char	*ft_content_env(char *str);
 void	*ft_calloc(size_t nitems, size_t size);
-void	ft_bzero(void *s, size_t n);
-size_t	ft_strlens(char *p, char a);
-char	*ft_strchrs(char *str, int c);
 void 	handle_new(int sig);
 void ft_exit(char **var, t_vars **env, t_vars **declare);
 #endif // !
